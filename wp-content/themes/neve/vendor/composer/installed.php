5619705bfdc534a5049a29c3f02352d527102f44
AntonioSellers <antonio.sf2004@gmail.com>
2024-04-05 14:20:27 +0200

    Primer commit

---
+++ wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve',
+        'pretty_version' => 'v3.8.5',
+        'version' => '3.8.5.0',
+        'reference' => '3315c1c7e5b8abf5d077f265455ea0f46a9e391e',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve' => array(
+            'pretty_version' => 'v3.8.5',
+            'version' => '3.8.5.0',
+            'reference' => '3315c1c7e5b8abf5d077f265455ea0f46a9e391e',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.17',
+            'version' => '3.3.17.0',
+            'reference' => 'b5698f21640d0156bd6777aa5c71e0704748755c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '68c93e1dc8f6e438f93e81ebdf632e6243e43e0f',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
